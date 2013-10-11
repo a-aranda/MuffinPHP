24a1b3087dd59ddc6ca76fd66b20a7511eae6c76
Alvaro Aranda <alvaroarandamunoz@gmail.com>
2013-10-11 12:13:10 +0200

    Adding different groups, adding one normal user called doe

--- src/CCUser/index.tpl.php
+++ src/CCUser/index.tpl.php
@@ -1,7 +1,9 @@
 <h1>User Profile</h1> 
 <ul> 
 <li><a href='<?=create_url(null, 'init')?>'>Init database, create tables and create default admin user</a> 
-<li><a href='<?=create_url(null, 'login', 'root/root')?>'>Login as root:root (should work)</a> 
+<li><a href='<?=create_url(null, 'login', 'root/root')?>'>Login as root:root (should work)</a>
+<li><a href='<?=create_url(null, 'login', 'doe/doe')?>'>Login as doe:doe (should work)</a> 
+<li><a href='<?=create_url(null, 'login', 'doe/root')?>'>Login as doe:root (should fail)</a>  
 <li><a href='<?=create_url(null, 'login', 'root@dbwebb.se/root')?>'>Login as root@dbwebb.se:root (should work)</a> 
 <li><a href='<?=create_url(null, 'login', 'admin/root')?>'>Login as admin:root (should fail, wrong akronym)</a> 
 <li><a href='<?=create_url(null, 'login', 'root/admin')?>'>Login as admin:root (should fail, wrong password)</a> 
--- src/CMUser/CMUser.php
+++ src/CMUser/CMUser.php
@@ -7,96 +7,116 @@
 class CMUser extends CObject implements IHasSQL {
 
 
-  /**
-   * Constructor
-   */
-  public function __construct() {
-    parent::__construct();
-  }
+/**
+ * Constructor
+ */
+public function __construct() {
+  parent::__construct();
+}
 
 
-  /**
-   * Implementing interface IHasSQL. Encapsulate all SQL used by this class.
-   *
-   * @param string $key the string that is the key of the wanted SQL-entry in the array.
-   */
-  public static function SQL($key=null) {
-    $queries = array(
-      'drop table user'    => "DROP TABLE IF EXISTS User;",
-      'create table user'  => "CREATE TABLE IF NOT EXISTS User (id INTEGER PRIMARY KEY, akronym TEXT KEY, name TEXT, email TEXT, password TEXT, created DATETIME default (datetime('now')));",
-      'insert into user'   => 'INSERT INTO User (akronym,name,email,password) VALUES (?,?,?,?);',
-      'check user password' => 'SELECT * FROM User WHERE password=? AND (akronym=? OR email=?);',
-     );
-    if(!isset($queries[$key])) {
-      throw new Exception("No such SQL query, key '$key' was not found.");
-    }
-    return $queries[$key];
-  }
+  /** 
+* Implementing interface IHasSQL. Encapsulate all SQL used by this class. 
+* 
+* @param string $key the string that is the key of the wanted SQL-entry in the array. 
+*/ 
+public static function SQL($key=null) { 
+  $queries = array( 
+    'drop table user' => "DROP TABLE IF EXISTS User;", 
+    'drop table group' => "DROP TABLE IF EXISTS Groups;", 
+    'drop table user2group' => "DROP TABLE IF EXISTS User2Groups;", 
+    'create table user' => "CREATE TABLE IF NOT EXISTS User (id INTEGER PRIMARY KEY, acronym TEXT KEY, name TEXT, email TEXT, password TEXT, created DATETIME default (datetime('now')));", 
+    'create table group' => "CREATE TABLE IF NOT EXISTS Groups (id INTEGER PRIMARY KEY, acronym TEXT KEY, name TEXT, created DATETIME default (datetime('now')));", 
+    'create table user2group' => "CREATE TABLE IF NOT EXISTS User2Groups (idUser INTEGER, idGroups INTEGER, created DATETIME default (datetime('now')), PRIMARY KEY(idUser, idGroups));", 
+    'insert into user' => 'INSERT INTO User (acronym,name,email,password) VALUES (?,?,?,?);', 
+    'insert into group' => 'INSERT INTO Groups (acronym,name) VALUES (?,?);', 
+    'insert into user2group' => 'INSERT INTO User2Groups (idUser,idGroups) VALUES (?,?);', 
+    'check user password' => 'SELECT * FROM User WHERE password=? AND (acronym=? OR email=?);', 
+    'get group memberships' => 'SELECT * FROM Groups AS g INNER JOIN User2Groups AS ug ON g.id=ug.idGroups WHERE ug.idUser=?;', 
+  ); 
+  if(!isset($queries[$key])) { 
+    throw new Exception("No such SQL query, key '$key' was not found."); 
+  } 
+    return $queries[$key]; 
+}
 
 
-  /**
-   * Init the database and create appropriate tables.
-   */
-  public function Init() {
-    try {
-      $this->db->ExecuteQuery(self::SQL('drop table user'));
-      $this->db->ExecuteQuery(self::SQL('create table user'));
-      $this->db->ExecuteQuery(self::SQL('insert into user'), array('root', 'The Administrator', 'root@dbwebb.se', 'root'));
-      $this->session->AddMessage('notice', 'Successfully created the database tables and created a default admin user as root:root.');
-    } catch(Exception$e) {
+/**
+* Init the database and create appropriate tables.
+*/
+public function Init() {
+  try {
+    $this->db->ExecuteQuery(self::SQL('drop table user2group'));
+    $this->db->ExecuteQuery(self::SQL('drop table group'));
+    $this->db->ExecuteQuery(self::SQL('drop table user'));
+    $this->db->ExecuteQuery(self::SQL('create table user'));
+    $this->db->ExecuteQuery(self::SQL('create table group'));
+    $this->db->ExecuteQuery(self::SQL('create table user2group'));
+    $this->db->ExecuteQuery(self::SQL('insert into user'), array('root', 'The Administrator', 'root@dbwebb.se', 'root'));
+    $idRootUser = $this->db->LastInsertId();
+    $this->db->ExecuteQuery(self::SQL('insert into user'), array('doe', 'John/Jane Doe', 'doe@dbwebb.se', 'doe'));
+    $idDoeUser = $this->db->LastInsertId();
+    $this->db->ExecuteQuery(self::SQL('insert into group'), array('admin', 'The Administrator Group'));
+    $idAdminGroup = $this->db->LastInsertId();
+    $this->db->ExecuteQuery(self::SQL('insert into group'), array('user', 'The User Group'));
+    $idUserGroup = $this->db->LastInsertId();
+    $this->db->ExecuteQuery(self::SQL('insert into user2group'), array($idRootUser, $idAdminGroup));
+    $this->db->ExecuteQuery(self::SQL('insert into user2group'), array($idRootUser, $idUserGroup));
+    $this->db->ExecuteQuery(self::SQL('insert into user2group'), array($idDoeUser, $idUserGroup));
+    $this->session->AddMessage('notice', 'Successfully created the database tables and created a default admin user as root:root and an ordinary user as doe:doe.');
+  } catch(Exception$e) {
       die("$e<br/>Failed to open database: " . $this->config['database'][0]['dsn']);
-    }
   }
+}
   
+/** 
+* Login by autenticate the user and password. Store user information in session if success. 
+* 
+* @param string $akronymOrEmail the emailadress or user akronym. 
+* @param string $password the password that should match the akronym or emailadress. 
+* @returns booelan true if match else false. 
+*/ 
+public function Login($akronymOrEmail, $password) { 
+  $user = $this->db->ExecuteSelectQueryAndFetchAll(self::SQL('check user password'), array($password, $akronymOrEmail, $akronymOrEmail)); 
+  $user = (isset($user[0])) ? $user[0] : null; 
+  unset($user['password']); 
+  if($user) { 
+    $user['groups'] = $this->db->ExecuteSelectQueryAndFetchAll(self::SQL('get group memberships'), array($user['id'])); 
+    $this->session->SetAuthenticatedUser($user); 
+    $this->session->AddMessage('success', "Welcome '{$user['name']}'."); 
+  } else { 
+    $this->session->AddMessage('notice', "Could not login, user does not exists or password did not match."); 
+  } 
+  return ($user != null); 
+}
 
-  /**
-   * Login by autenticate the user and password. Store user information in session if success.
-   *
-   * @param string $akronymOrEmail the emailadress or user akronym.
-   * @param string $password the password that should match the akronym or emailadress.
-   * @returns booelan true if match else false.
-   */
-  public function Login($akronymOrEmail, $password) {
-    $user = $this->db->ExecuteSelectQueryAndFetchAll(self::SQL('check user password'), array($password, $akronymOrEmail, $akronymOrEmail));
- 		$user = (isset($user[0])) ? $user[0] : null;
- 		unset($user['password']);
-    if($user) {
-      $this->session->SetAuthenticatedUser($user);
-      $this->session->AddMessage('success', "Welcome '{$user['name']}'.");
-    } else {
-      $this->session->AddMessage('notice', "Could not login, user does not exists or password did not match.");
-    }
-    return ($user != null);
-  }
-  
+/**
+ * Logout.
+ */
+public function Logout() {
+  $this->session->UnsetAuthenticatedUser();
+  $this->session->AddMessage('success', "You have logged out.");
+}
 
-  /**
-   * Logout.
-   */
-  public function Logout() {
-    $this->session->UnsetAuthenticatedUser();
-    $this->session->AddMessage('success', "You have logged out.");
-  }
-  
 
-  /**
-   * Does the session contain an authenticated user?
-   *
-   * @returns boolen true or false.
-   */
-  public function IsAuthenticated() {
-    return ($this->session->GetAuthenticatedUser() != false);
-  }
-  
-  
-  /**
-   * Get profile information on user.
-   *
-   * @returns array with user profile or null if anonymous user.
-   */
-  public function GetUserProfile() {
-    return $this->session->GetAuthenticatedUser();
-  }
+/**
+ * Does the session contain an authenticated user?
+ *
+ * @returns boolen true or false.
+ */
+public function IsAuthenticated() {
+  return ($this->session->GetAuthenticatedUser() != false);
+}
+
+
+/**
+ * Get profile information on user.
+ *
+ * @returns array with user profile or null if anonymous user.
+ */
+public function GetUserProfile() {
+  return $this->session->GetAuthenticatedUser();
+}
   
   
 }
