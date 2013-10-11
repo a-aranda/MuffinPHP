3639ead7f58ced22a872392b7e77dfbe6902ee39
Alvaro Aranda <alvaroarandamunoz@gmail.com>
2013-10-11 15:03:11 +0200

    displaying available controllers using reflection in the index controller

--- src/CCIndex/CCIndex.php
+++ src/CCIndex/CCIndex.php
@@ -4,18 +4,42 @@
 * 
 * @package MuffinPHP Core
 */ 
-class CCIndex implements IController { 
+class CCIndex extends CObject implements IController { 
+
+/**
+   * Constructor
+   */
+  public function __construct() {
+    parent::__construct();
+  }
 
 /** 
 * Implementing interface IController. All controllers must have an index action. 
 */ 
 public function Index() { 
-	global $muff; 
-	$muff->data['title'] = "The Index Controller"; 
-	$muff->data['header'] = '<h1>Header: Lydia</h1>';
-	$muff->data['main'] = "";
-    $muff->data['footer'] = '<p>Alvaro Aranda on MuffinPHP</p>';
-    $muff->data['title'] = "This is the title";
-} 
+	$this->views->SetTitle('Index Controller'); 
+	$this->views->AddInclude(__DIR__ . '/index.tpl.php', array('menu'=>$this->Menu())); 
+}
+
+/** 
+* A menu that shows all available controllers/methods 
+*/ 
+private function Menu() { 
+	$items = array(); 
+	foreach($this->config['controllers'] as $key => $val) { 
+		if($val['enabled']) { 
+			$rc = new ReflectionClass($val['class']); 
+			$items[] = $key; 
+			$methods = $rc->getMethods(ReflectionMethod::IS_PUBLIC); 
+			foreach($methods as $method) { 
+				if($method->name != '__construct' && $method->name != '__destruct' && $method->name != 'Index') { 
+					$items[] = "$key/" . mb_strtolower($method->name); 
+				} 
+			} 
+		} 
+	} 
+	return $items; 
+}
+
 
 }
